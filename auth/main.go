7439370a482934087d3b57158fa54e3c16d90ee7
Nicholas Chang <nicholaschang@Nicholass-Mac-Studio.local>
2022-09-22 17:27:38 +1000

    registration working and login seems to be working

--- auth/main.go
+++ auth/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 
@@ -12,13 +13,19 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func AuthMiddleware(handler http.Handler) http.Handler {
+const cookieCharacters = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var randSeed *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func MockAuthMiddleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		// mock extraction of cookie
 		var testChainJSONStr string
 		yummers := r.Cookies()
-		for cKey, cValue := range yummers {
-			testChainJSONStr += fmt.Sprintf("field %d: %v\n", cKey, *cValue)
+		for idx, ptrCookie := range yummers {
+			testChainJSONStr += fmt.Sprintf("field %d: %v\n", idx, *ptrCookie)
 		}
 		/*
 			Marshal encodes any Go value into a byte slice which represents JSON
@@ -35,13 +42,53 @@ func AuthMiddleware(handler http.Handler) http.Handler {
 	})
 }
 
-func UserExists(searchParams bson.D, uCollection *mongo.Collection) (bool, error) {
-	// can be optimised with opts refer to documentation:
-	// https://pkg.go.dev/go.mongodb.org/mongo-driver/mongo#Collection.FindOne
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-	var user bson.M
-	err := uCollection.FindOne(ctx, searchParams).Decode(&user)
+/*
+Marshal encodes any Go value into a byte slice which represents JSON
+Unmarshal decodes a byte slice (which is a JSON representation) into a Go value
+Encoding and Decoding similar idea to Marshal and Unmarshal but implementation
+isn't the exact same.
+
+purpose of chaining middlewares; pass same w and r through multiple functions (handlers).
+so call .ServeHTTP on next handler; ServeHTTP takes care of responding to a HTTP request.
+*/
+func AuthMiddleware(sCollection *mongo.Collection) func(http.Handler) http.Handler {
+	return func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			// actual auth logic
+			ptrCookieSlice := r.Cookies()
+			fmt.Printf("cookies: %v\n", ptrCookieSlice)
+			var sessionID string
+			for _, ptrCookie := range ptrCookieSlice {
+				if (*ptrCookie).Name == "session-id" {
+					sessionID = (*ptrCookie).Value
+				}
+			}
+			if len(sessionID) > 0 {
+				// can't really dispatch into another goroutine because rest of this function
+				// has to wait for result of Exists(), so has to block anyways
+				exists, err := Exists(bson.D{{Key: "session", Value: sessionID}}, sCollection)
+				if err != nil {
+					log.Fatal(err) // something wrong with db
+				}
+				switch exists {
+				case true:
+					json.NewEncoder(w).Encode(fmt.Sprintf("session id: %s", sessionID))
+					handler.ServeHTTP(w, r)
+				case false:
+					json.NewEncoder(w).Encode(fmt.Sprintf("session id: %s invalid", sessionID))
+				}
+			} else {
+				json.NewEncoder(w).Encode("no session id, check your cookies")
+			}
+		})
+	}
+}
+
+func Exists(searchParams bson.D, collection *mongo.Collection) (bool, error) {
+	// no timeout context because NEED to find whether or not user or session exists
+	var result bson.M
+	err := collection.FindOne(context.TODO(), searchParams).Decode(&result)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
 			return false, nil
@@ -53,32 +100,54 @@ func UserExists(searchParams bson.D, uCollection *mongo.Collection) (bool, error
 	return true, nil
 }
 
+// maybe sometime later use generics with Exist since so similar (FindOne())
+func FindSession(searchSession bson.D, collection *mongo.Collection) (string, error) {
+	// result will just be a map e.g. access username value by result["user"]
+	var result bson.M
+	err := collection.FindOne(context.TODO(), searchSession).Decode(&result)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return "", nil
+		}
+		log.Fatal(err)
+		return "", err
+	}
+	// assumes all values for key "session" will be stored as string
+	return result["session"].(string), nil
+}
+
 // inserts doc into sessions collection. doc is the current session of authed user.
 // is spawned as a goroutine and creates a session. then this goroutine will spawn
 // another to delete session doc once timed out.
-func CreateNewSession(channel chan<- *mongo.InsertOneResult, userInfo map[string]string,
+func CreateNewSession(channel chan<- string, userInfo map[string]string,
 	sCollection *mongo.Collection) {
 	// in future could consider maybe reddis or just a global slice for speed
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 	// empty initialisation rather than nil (var sessionDocument bson.M)
 	sessionDocument := make(bson.M)
-	for keyField, valueUser := range userInfo {
-		sessionDocument[keyField] = valueUser
+	sessionDocument["user"] = userInfo["user"]
+	// generate a sessionID to be set in client's Cookie header
+	sessionID := make([]byte, 64)
+	for idx := range sessionID {
+		sessionID[idx] = cookieCharacters[randSeed.Intn(len(cookieCharacters))]
 	}
-	newSession, err := sCollection.InsertOne(ctx, sessionDocument)
+	sessionDocument["session"] = string(sessionID)
+	// insert document (a session) into mongoDB
+	_, err := sCollection.InsertOne(ctx, sessionDocument)
 	if err != nil {
 		fmt.Println("mongo error inserting new session document")
+		return
 	}
-	// another goroutine so no issues with blocking statement: channel <- newSession
+	// another goroutine so won't block the statement: channel <- string(sessionID)
 	go DeleteSessionTimeout(sessionDocument, sCollection)
-	channel <- newSession
+	channel <- string(sessionID)
 }
 
 // deletes session doc from mongo sessions collection. is spawned as a goroutine
 // by createNewSession so that it can clean up after itself.
 func DeleteSessionTimeout(session bson.M, sCollection *mongo.Collection) {
-	timeoutChan := time.NewTimer(time.Duration(30 * time.Second))
+	timeoutChan := time.NewTimer(time.Duration(20 * time.Second))
 	<-timeoutChan.C // halt execution of this goroutine here until timeout
 	// doesn't matter how long it takes, session has to be deleted
 	_, err := sCollection.DeleteOne(context.TODO(), session)
@@ -88,16 +157,15 @@ func DeleteSessionTimeout(session bson.M, sCollection *mongo.Collection) {
 }
 
 // for now almost same implementation as creating new session for authed user.
-// just don't dispatch a deletion scheduled after 30 second
+// just don't dispatch a deletion scheduled after 90 second
+// no timeout on InsertOne either because important that user is registered in db
 func CreateNewUser(channel chan<- *mongo.InsertOneResult, userInfo map[string]string,
 	uCollection *mongo.Collection) {
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
 	userDocument := make(bson.M)
 	for keyField, valueUser := range userInfo {
 		userDocument[keyField] = valueUser
 	}
-	newUser, err := uCollection.InsertOne(ctx, userDocument)
+	newUser, err := uCollection.InsertOne(context.TODO(), userDocument)
 	if err != nil {
 		fmt.Println("mongo error inserting new user record")
 	}
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	handlers "github.com/gorilla/handlers"
 	mux "github.com/gorilla/mux"
@@ -99,17 +100,18 @@ func chainMiddleware(baseHandler http.Handler,
 }
 
 /*
-search for existing user: lookForUser()
-if not existing then create new user in db: go createNewUser()
-then create a new session: go createNewSession()
+search for existing user
+if not existing then create new user in db
+then create a new session
 https://stackoverflow.com/questions/43021058/golang-read-request-body-multiple-times
 */
 func register(w http.ResponseWriter, r *http.Request) {
+	// set http headers for when need to send response back
 	w.Header().Set("Content-Type", "application/json")
-	// fmt.Printf("body value: %v, body type: %T", (*r).Body, (*r).Body)
 
-	// pull out json from request body into a byte slice then into a nice map
+	// pull out json from request body into a byte slice then into a nice map for late use
 	var userInputMap map[string]string
+	// fmt.Printf("body value: %v, body type: %T", (*r).Body, (*r).Body)
 	userInputBytes, err := io.ReadAll(r.Body)
 	if err != nil {
 		json.NewEncoder(w).Encode("error: no body could be read")
@@ -118,7 +120,6 @@ func register(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("couldn't unmarshal the byte slice representation of JSON into a map")
 	}
-
 	// construct bson document describing the user credentials passed in by body
 	var userAsBSOND bson.D
 	for key, value := range userInputMap {
@@ -126,7 +127,7 @@ func register(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// this has to block, because whether or not user exists determines course of action
-	found, err := auth.UserExists(userAsBSOND, userCollection)
+	found, err := auth.Exists(userAsBSOND, userCollection)
 	if err != nil {
 		fmt.Println("could not complete search for user in sessions collection")
 	}
@@ -136,30 +137,178 @@ func register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// run the two creates using goroutines to leverage context switching while
-	// the asynchronous InsertOne is blocking (spawn goroutines for performance)
-	// userChan := make(chan *mongo.InsertOneResult)
-	sessionChan := make(chan *mongo.InsertOneResult)
-	// go createNewUser(userChan, userInputMap)
+	// run the two creates using goroutines to leverage context switching while InsertOne
+	// is waiting (InsertOne blocks rest of Create...()); spawn goroutines for performance
+	userChan := make(chan *mongo.InsertOneResult)
+	sessionChan := make(chan string)
+	go auth.CreateNewUser(userChan, userInputMap, userCollection)
 	go auth.CreateNewSession(sessionChan, userInputMap, sessionCollection)
-	json.NewEncoder(w).Encode(fmt.Sprintln(*(<-sessionChan)))
+
+	// prepare response while considering potential timeout
+	// if after 2 seconds both sessionID and user result not provided, timeout
+	var msg, cookieName string
+	for numAppended := 0; numAppended < 2; numAppended++ {
+		select {
+		case sess := <-sessionChan:
+			msg += fmt.Sprintf("session: %v\n", string(sess))
+			cookieName = fmt.Sprintf("%v", string(sess))
+		case user := <-userChan:
+			msg += fmt.Sprintf("user: %v\n", *user)
+		case <-time.After(2 * time.Second):
+			msg = "registration timed out: invalid user info or backend issue"
+			numAppended = 2 // needed to break out of for loop
+		}
+	}
+	// ask client to set a cookie, so set Set-Cookie in header according to mdn docs
+	w.Header().Set("Set-Cookie", fmt.Sprintf("session-id=%s", cookieName))
+	json.NewEncoder(w).Encode(msg)
+}
+
+/*
+1. dispatch goroutine: search for user in user collection
+2. dispatch goroutine: search for session using field "user" instead of "session"
+to be considered 'logged in':
+  - 1. exists and 2. not exists: create session document in db, Set-Cookie in response
+  - 1. exists and 2. exists:
+    if request cookie different to generated session cookie by mongo, Set-Cookie
+*/
+func login(w http.ResponseWriter, r *http.Request) {
+	// set http headers for when need to send response back
+	w.Header().Set("Content-Type", "application/json")
+
+	// pull out json from request body into a byte slice then into a nice map for later use
+	var userInputMap map[string]string
+	// fmt.Printf("body value: %v, body type: %T", (*r).Body, (*r).Body)
+	userInputBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		json.NewEncoder(w).Encode("error: no body could be read")
+	}
+	err = json.Unmarshal(userInputBytes, &userInputMap)
+	if err != nil {
+		fmt.Println("couldn't unmarshal the byte slice representation of JSON into a map")
+	}
+	// construct bson document describing the user credentials passed in by body
+	var userAsBSOND bson.D
+	for key, value := range userInputMap {
+		userAsBSOND = append(userAsBSOND, bson.E{Key: key, Value: value})
+	}
+	fmt.Printf("user: %v", userAsBSOND)
+	var sessionAsBSOND bson.D
+	sessionAsBSOND = append(sessionAsBSOND, bson.E{Key: "user", Value: userInputMap["user"]})
+	fmt.Printf("session: %v", sessionAsBSOND)
+
+	/* run the two searches as specified above to leverage context switching; performance.
+	e.g. FindOne blocks rest of Exists(). blocking because in waiting state (wait for mongo);
+	can context switch to another goroutine */
+	// user search goroutine tell login goroutine whether or not user is valid in db
+	grlchangru := make(chan bool)
+	// session manager goroutine sends login goroutine session string to be set in cookie
+	// this can only be done if user is valid in db
+	grlchangrs := make(chan string)
+	// user search goroutine tell session search goroutine whether or not user is valid in db
+	grschangru := make(chan bool)
+	// session search goroutine sends session manager goroutine result of its search
+	grssearch := make(chan string)
+	// actually search for user existence in user collection
+	go func() {
+		userExists, err := auth.Exists(userAsBSOND, userCollection)
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch userExists {
+		case false:
+			grlchangru <- userExists
+		case true:
+			grschangru <- userExists
+		}
+	}()
+	// actually search for session existence in session collection
+	go func() {
+		// change this to find the session and return it, or return "" if not exists
+		session, err := auth.FindSession(sessionAsBSOND, sessionCollection)
+		if err != nil {
+			log.Fatal(err)
+		}
+		grssearch <- session // don't need process anything else, so block until read out
+	}()
+	// coordinate result of above two searches
+	go func() {
+		// need to wait for below two info/guarantee before move on; blocking inevitable
+		// has to block and doesn't matter the order of the two
+		// the one that blocks for longest is time taken to get both results
+		// instead of waiting for user search + session search
+		sessionSearchResult := <-grssearch
+		<-grschangru // only time receiving on this channel is if user does exist
+		// session already exists for this valid user
+		if len(sessionSearchResult) > 0 {
+			// pump string back to login goroutine straight away
+			grlchangrs <- sessionSearchResult
+		} else {
+			// else create new session doc in db and pump back to login goroutine
+			auth.CreateNewSession(grlchangrs, userInputMap, sessionCollection)
+		}
+	}()
+
+	select {
+	case session := <-grlchangrs:
+		// a valid session in db for a valid user: prepare write response 'logged in'
+		// check if client already has cookie in request header
+		ptrCookieSlice := r.Cookies()
+		fmt.Printf("cookies: %v\n", ptrCookieSlice)
+		var sessionIDCookie string
+		for _, ptrCookie := range ptrCookieSlice {
+			if (*ptrCookie).Name == "session-id" {
+				sessionIDCookie = (*ptrCookie).Value
+			}
+		}
+		// compare sessionID with session that will need to be returned
+		if sessionIDCookie != session {
+			// if diff, session generated by mongo will be flushed back to client
+			w.Header().Set("Set-Cookie", fmt.Sprintf("session-id=%s", session))
+		}
+		// else assumed "Set-Cookie" header already set properly
+		json.NewEncoder(w).Encode("logged in")
+	case <-grlchangru: // only time something sent into grlchangru is if user not exist
+		// bad news no more waiting to receive on other channels
+		// rewrite Set-Cookie headers if somehow a valid session was set by above
+		w.Header().Set("Set-Cookie", "session-id=oopsSomebodysUnauthenticated")
+		// write straight back to client that user credentials wrong
+		json.NewEncoder(w).Encode("wrong login credentials")
+		return
+	}
+}
+
+func sessionfind(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var result bson.M
+	_ = sessionCollection.FindOne(context.TODO(), bson.D{{Key: "user", Value: "tester"}}).Decode(&result)
+	json.NewEncoder(w).Encode(fmt.Sprintf("result: %v, just username: %s", result, result["user"]))
 }
 
 func main() {
 	router := mux.NewRouter()
 	// CORS access for frontend running on port 3000
+	// use http://localhost:3000 for testing
 	router.Use(handlers.CORS(handlers.AllowedOrigins([]string{"*"})))
 
 	apiV1Router := router.PathPrefix("/api/v1").Subrouter()
+	v1AuthRouter := apiV1Router.PathPrefix("/auth").Subrouter()
+	v1ContentRouter := apiV1Router.PathPrefix("/content").Subrouter()
+
 	apiV1Router.HandleFunc("/test", testHelloWorld).Methods("GET")
-	// apiV1Router.Handle("/chain-test", testChain(http.HandlerFunc(testHelloWorld))).Methods("GET")
+	apiV1Router.HandleFunc("/test-session-find", sessionfind).Methods("GET")
+
 	// /chain-test is only 'protected' route
-	apiV1Router.
-		// type http.HandlerFunc implements serveHTTP method; can be passed in when parameter expected
-		// to implement http.Handler interface
-		Handle("/chain-test", chainMiddleware(http.HandlerFunc(readCountAuthedUsers), auth.AuthMiddleware)).
+	v1ContentRouter.
+		// type http.HandlerFunc implements serveHTTP method;
+		// can be passed in when parameter expected to implement http.Handler interface
+		Handle("/chain-test",
+			chainMiddleware(http.HandlerFunc(readCountAuthedUsers),
+				auth.AuthMiddleware(sessionCollection))).
 		Methods("GET")
-	apiV1Router.HandleFunc("/register", register).Methods("POST")
+
+	v1AuthRouter.HandleFunc("/register", register).Methods("POST")
+	v1AuthRouter.HandleFunc("/login", login).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":3001", router))
 }
